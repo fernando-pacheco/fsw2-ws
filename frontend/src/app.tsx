1113e1a6e652b8608d1ab20bce59a17870dfc060
fernando-pacheco <fernandopachecopx@gmail.com>
2024-11-06 11:39:58 -0300

    Correção da base frontend

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,8 @@
 export function App() {
 
   return (
-    <>
+    <div className="text-3xl text-cyan-600 font-semibold flex justify-center items-center min-h-screen w-full">
       Hello World
-    </>
+    </div>
   )
 }
-
-export default App
